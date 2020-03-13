1dc07e04b9001eff51b8e06f9ee9109cba782396
ever <ever1337@protonmail.ch>
2020-03-13 14:40:35 -0700

    pull in more dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,41 @@
 use ggez::{
-    Context,
-    ContextBuilder,
-    GameResult,
-    timer,
-    event::{
-        self,
-        EventHandler
-    },
-    graphics::{
-        self,
-        Rect,
-    }
+    event::{self, EventHandler},
+    graphics::{self, Rect},
+    timer, Context, ContextBuilder, GameResult, conf, filesystem,
 };
 
-use std::time::Duration;
+use gfx_glyph::{Section, GlyphBrushBuilder};
 
-#[macro_use] extern crate log;
+use std::{
+    time::Duration,
+    io::{Read, Write},
+    env,
+    path,
+    str
+};
+
+#[macro_use]
+extern crate log;
 extern crate env_logger;
 
 enum Scene {
+    Loading,
     MainMenu,
     Game,
 }
 
 struct GameState {
     dt: Duration, //delta time
+    fps: f64,
+    scene: Scene,
 }
 
 impl GameState {
     pub fn new(_ctx: &mut Context) -> GameState {
         GameState {
-            dt: Duration::new(0, 0), 
+            dt: Duration::new(0, 0),
+            fps: 0f64,
+            scene: Scene::Loading,
         }
     }
 }
@@ -38,6 +43,7 @@ impl GameState {
 impl EventHandler for GameState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
         self.dt = timer::delta(_ctx);
+        self.fps = timer::fps(_ctx);
 
         Ok(())
     }
@@ -53,16 +59,25 @@ impl EventHandler for GameState {
 fn main() {
     env_logger::init();
 
-    let (mut ctx, mut event_loop) = ContextBuilder::new("Victrix", "Ever")
-        .build()
-        .expect("Failed to create ggez context");
-    
+    let mut cb = ContextBuilder::new("Victrix", "Ever");
+
+    //set up resource directories
+    if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+        let mut path = path::PathBuf::from(manifest_dir);
+        path.push("resources");
+        info!("Adding path {:?}", path);
+        cb = cb.add_resource_path(path); 
+    }
+
+    let (mut ctx, mut event_loop) = cb.build()
+        .expect("Failed to build context.");
+
     graphics::set_screen_coordinates(&mut ctx, Rect::new(0.0, 0.0, 1.0, 1.0)).unwrap();
 
     let mut game = GameState::new(&mut ctx);
 
     match event::run(&mut ctx, &mut event_loop, &mut game) {
         Ok(_) => println!("Game exiting"),
-        Err(e) => println!("Error: {}", e)
+        Err(e) => println!("Error: {}", e),
     }
 }
