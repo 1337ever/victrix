d9c1dcfdfc9508d90f54dbbda60d0ec1b5a73860
ever <ever1337@protonmail.ch>
2020-03-19 15:07:04 -0700

    move from using piston_window to using glium

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,24 @@
+#[macro_use]
+extern crate conrod_core;
+
 use std::fs;
 
 use ron::de::from_str;
 use serde::Deserialize;
 
-extern crate conrod_core;
-extern crate conrod_piston;
-extern crate piston_window;
-
-use piston_window::texture::UpdateTexture;
-use piston_window::OpenGL;
-use piston_window::{Flip, G2d, G2dTexture, Texture, TextureSettings};
-use piston_window::{PistonWindow, UpdateEvent, Window, WindowSettings};
+use conrod_core::{widget, Colorable, Positionable, Widget};
+use glium::Surface;
 
 mod history;
+mod support;
 
 //Struct for config data like game settings
 #[derive(Debug, Deserialize)]
 pub struct Config {
-    pub window_size: [u32; 2],
+    pub size: [f64; 2],
     pub fullscreen: bool,
     pub vsync: bool,
-    pub antialiasing: u8,
+    pub antialiasing: u16,
     pub font: String,
 
     pub volume: f32,     //float between 0 and 1
@@ -55,6 +53,7 @@ pub struct Game {
     scene: Scene,
     date: u8,
 }
+widget_ids!(struct Ids { canvas, list });
 
 fn main() {
     let mut game = Game {
@@ -63,89 +62,110 @@ fn main() {
         date: 0,
     };
 
-    /*let theme = conrod_core::Theme {
-        name: "Default theme".to_string(),
-        padding: Padding::none(),
-        x_position: Position::Relative(Relative::Align(Align::Start), None),
-        y_position: Position::Relative(Relative::Direction(Direction::Backwards, 20.0), None),
-        background_color: conrod_core::color::DARK_CHARCOAL,
-        shape_color: conrod_core::color::LIGHT_CHARCOAL,
-        border_color: conrod_core::color::BLACK,
-        border_width: 0.0,
-        label_color: conrod_core::color::WHITE,
-        font_id: None,
-        font_size_large: 26,
-        font_size_medium: 18,
-        font_size_small: 12,
-        widget_styling: conrod_core::theme::StyleMap::default(),
-        mouse_drag_threshold: 0.0,
-        double_click_threshold: std::time::Duration::from_millis(500),
-    };*/
+    //Create window
+    let size = glium::glutin::dpi::LogicalSize::new(game.config.size[0], game.config.size[1]);
+    let mut events_loop = glium::glutin::EventsLoop::new();
+    let window = glium::glutin::WindowBuilder::new()
+        .with_title("Victrix")
+        .with_dimensions(size);
+    let context = glium::glutin::ContextBuilder::new()
+        .with_vsync(game.config.vsync)
+        .with_multisampling(game.config.antialiasing);
+    let display =
+        glium::Display::new(window, context, &events_loop).expect("Failed to create display");
+    let display = support::GliumDisplayWinitWrapper(display);
 
-    let mut window: PistonWindow = WindowSettings::new("Victrix", game.config.window_size)
-        .exit_on_esc(true)
-        .samples(game.config.antialiasing)
-        .vsync(game.config.vsync)
-        .fullscreen(game.config.fullscreen)
-        .build()
-        .expect("Failed to create window with given configuration.");
-
-    //construct UI
-    let mut ui = conrod_core::UiBuilder::new([
-        game.config.window_size[0] as f64,
-        game.config.window_size[1] as f64,
-    ])
-    .build();
-
-    //Load font
-    ui.fonts
-        .insert_from_file(game.config.font)
-        .expect("Unable to load font.");
-
-    //create texture to cache text
-    let mut text_vertex_data: Vec<u8> = Vec::new();
-    let (mut glyph_cache, mut text_texture_cache) = {
-        const SCALE_TOLERANCE: f32 = 0.1;
-        const POSITION_TOLERANCE: f32 = 0.1;
-        let cache = conrod_core::text::GlyphCache::builder()
-            .dimensions(game.config.window_size[0], game.config.window_size[1])
-            .scale_tolerance(SCALE_TOLERANCE)
-            .position_tolerance(POSITION_TOLERANCE)
+    //Construct UI
+    let mut ui =
+        conrod_core::UiBuilder::new([game.config.size[0] as f64, game.config.size[1] as f64])
             .build();
-        let buffer_len = game.config.window_size[0] as usize * game.config.window_size[1] as usize;
-        let init = vec![128; buffer_len];
-        let settings = TextureSettings::new();
-        let texture_context = &mut window.create_texture_context();
-        let texture = G2dTexture::from_memory_alpha(
-            texture_context,
-            &init,
-            game.config.window_size[0],
-            game.config.window_size[1],
-            &settings,
-        )
-        .unwrap();
-        (cache, texture)
-    };
 
-    //let mut events = Events::new(EventSettings::new());
+    //Generate widget IDs
+    let ids = Ids::new(ui.widget_id_generator());
 
-    while let Some(event) = window.next() {
-        let size = window.size();
-        let (win_w, win_h) = (
-            size.width as conrod_core::Scalar,
-            size.height as conrod_core::Scalar,
-        );
-        if let Some(e) = conrod_piston::event::convert(event.clone(), win_w, win_h) {
-            ui.handle_event(e);
+    //Add a font to the font::Map
+    let font_path = "resources/OpenSans-Regular.ttf";
+    ui.fonts
+        .insert_from_file(font_path)
+        .expect("Failed to insert font");
+
+    //convert conrod primitives into commands for drawing to glium Surface
+    let mut renderer = conrod_glium::Renderer::new(&display.0).expect("Failed to create Renderer");
+
+    let image_map = conrod_core::image::Map::<glium::texture::Texture2d>::new();
+
+    let mut list = vec![true; 16];
+
+    // Poll events from the window.
+    let mut event_loop = support::EventLoop::new();
+    'main: loop {
+        // Handle all events.
+        for event in event_loop.next(&mut events_loop) {
+            // Use the `winit` backend feature to convert the winit event to a conrod one.
+            if let Some(event) = support::convert_event(event.clone(), &display) {
+                ui.handle_event(event);
+                event_loop.needs_update();
+            }
+
+            match event {
+                glium::glutin::Event::WindowEvent { event, .. } => match event {
+                    // Break from the loop upon `Escape`.
+                    glium::glutin::WindowEvent::CloseRequested
+                    | glium::glutin::WindowEvent::KeyboardInput {
+                        input:
+                            glium::glutin::KeyboardInput {
+                                virtual_keycode: Some(glium::glutin::VirtualKeyCode::Escape),
+                                ..
+                            },
+                        ..
+                    } => break 'main,
+                    _ => (),
+                },
+                _ => (),
+            }
+        }
+
+        set_ui(ui.set_widgets(), &mut list, &ids);
+
+        // Render the `Ui` and then display it on the screen.
+        if let Some(primitives) = ui.draw_if_changed() {
+            renderer.fill(&display.0, primitives, &image_map);
+            let mut target = display.0.draw();
+            target.clear_color(0.0, 0.0, 0.0, 1.0);
+            renderer.draw(&display.0, &mut target, &image_map).unwrap();
+            target.finish().unwrap();
         }
     }
-    /*while let Some(e) = events.next(&mut window) {
-        if let Some(args) = e.render_args() {
-            app.render(&args);
-        }
+}
 
-        if let Some(args) = e.update_args() {
-            app.update(&args);
+// Declare the `WidgetId`s and instantiate the widgets.
+fn set_ui(ref mut ui: conrod_core::UiCell, list: &mut [bool], ids: &Ids) {
+    use conrod_core::{widget, Colorable, Labelable, Positionable, Sizeable, Widget};
+
+    widget::Canvas::new()
+        .color(conrod_core::color::DARK_CHARCOAL)
+        .set(ids.canvas, ui);
+
+    let (mut items, scrollbar) = widget::List::flow_down(list.len())
+        .item_size(50.0)
+        .scrollbar_on_top()
+        .middle_of(ids.canvas)
+        .wh_of(ids.canvas)
+        .set(ids.list, ui);
+
+    while let Some(item) = items.next(ui) {
+        let i = item.i;
+        let label = format!("item {}: {}", i, list[i]);
+        let toggle = widget::Toggle::new(list[i])
+            .label(&label)
+            .label_color(conrod_core::color::WHITE)
+            .color(conrod_core::color::LIGHT_BLUE);
+        for v in item.set(toggle, ui) {
+            list[i] = v;
         }
-    }*/
+    }
+
+    if let Some(s) = scrollbar {
+        s.set(ui)
+    }
 }
